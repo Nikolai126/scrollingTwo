716af7948d4197e89d2bd34c38679cb2e23951a6
NikolaiRozhkov <agent-nik126@mail.ru>
2021-10-23 23:20:26 +0300

    lastCommit

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,37 @@
-const title_height = 64;
-const userHeight = 270;
+const test = {
+  run: function () {
+    const title_height = 64;
+    const userHeight = 270;
 
-this.addEventListener('click', function(event) {
-  let element = document.getElementById(event.target.dataset.section).offsetTop;
-  window.scrollTo({
-    top: element - title_height,
-    behavior: "smooth"
-  });
-  if (event.target.dataset.section === 'five') {
-    document.getElementById('five').scrollIntoView({block: "start", behavior: "smooth"});
-  }
-});
-
-window.onscroll = function() {
-  const scrolled = window.pageYOffset;
-  console.log(scrolled)
-  if (scrolled > userHeight) {
-        document.getElementById('scroll-top').classList.add('visible'); 
-  } 
-  else {
-        document.getElementById('scroll-top').classList.remove('visible');
+      document.querySelector('.menu').addEventListener('click', function(event) {
+      let element = document.getElementById(event.target.dataset.section).offsetTop;
+      window.scrollTo({
+        top: element - title_height,
+        behavior: "smooth"
+      });
+      if (event.target.dataset.section === 'five') {
+        document.getElementById('five').scrollIntoView({block: "start", behavior: "smooth"});
+      }
+    });
+    
+    window.addEventListener('scroll', function() {
+      const scrolled = window.pageYOffset;
+      console.log(scrolled)
+      if (scrolled > userHeight) {
+            document.getElementById('scroll-top').classList.add('visible'); 
+      } 
+      else {
+            document.getElementById('scroll-top').classList.remove('visible');
+      }
+    });
+    
+    document.getElementById('scroll-top').addEventListener('click', function(event) {
+      window.scrollTo({
+      top:0,
+      behavior: "smooth"
+      });
+    });
   }
 }
 
-document.getElementById('scroll-top').onclick = function () {
-  window.scrollTo({
-  top:0,
-  behavior: "smooth"
-});
-}
+test.run();
